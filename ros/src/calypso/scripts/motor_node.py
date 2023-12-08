11150a25747b128feefba8ffefb9c6028a2d27df
abourida28 <omarabourida2015@gmail.com>
2023-12-09 00:49:59 +0200

    Added motor driving functionality

--- ros/src/calypso/scripts/motor_node.py
+++ ros/src/calypso/scripts/motor_node.py
@@ -2,22 +2,57 @@
 
 import rospy
 from calypso.msg import joystick
+from board import SCL, SDA
+import busio
+from adafruit_pca9685 import PCA9685
+import RPi.GPIO as GPIO
 
-# Assumption that forward is +ve and backward is -ve for all motors with next configuration
+class Motor:
+    def __init__(self, index):
+        self.index = index
+
+    def setSpeed(self, speed, rotatesForward):
+        pca.channels[self.index].duty_cycle = speed
+        GPIO.output(self.index, rotatesForward)
+
+
+# Assumption that forward is ture and backward is -false for all motors with next configuration
 # m1 m2
 # m3 m4
 # m5 m6
-SPEED_MULTIPLIER = 100
+i2c_bus = busio.I2C(SCL, SDA)
+pca = PCA9685(i2c_bus)
+pca.frequency = 60
+SPEED_MULTIPLIER = 0xFFFF
+
+GPIO.setmode(GPIO.BOARD)
+directionPins = [5, 6, 13, 19, 26, 20]
+GPIO.setup(directionPins, GPIO.OUT)
+
+motors = list()
+
+for i in range(6):
+    motors.append(Motor(i))
 
 def listenerCallback(joystickValues):
     if (abs(joystickValues.lx) > abs(joystickValues.ly)):
         # Turn left or right
         m1 = m3 = m5 = joystickValues.lx * SPEED_MULTIPLIER
         m2 = m4 = m6 = -1 * joystickValues.lx * SPEED_MULTIPLIER
+        speed = joystickValues.lx * SPEED_MULTIPLIER
+
+        for i in range(0, 6, 2):
+            motors[i].setSpeed(speed, joystickValues.lx > 0)
+
+        for i in range(1, 6, 2):
+            motors[i].setSpeed(speed, joystickValues.lx < 0)
 
     else:
         # Move front or back
         m1 = m2 = m3 = m4 = m5 = m6 = joystickValues.ly * SPEED_MULTIPLIER
+        speed = joystickValues.ly * SPEED_MULTIPLIER
+        for i in range(6):
+            motors[i].setSpeed(speed, joystickValues.ly > 0)
 
     rospy.loginfo("m1 = %d \tm2 = %d\nm3 = %d \tm4 = %d\nm5 = %d \tm6 = %d\n\n" %(m1, m2, m3, m4, m5, m6))
 
